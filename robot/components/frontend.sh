011131eb1ac3efe2eea749c6786a2aa4e38d09a8
Mounika2998 <mounikadamarla2429@gmail.com>
2023-03-13 22:42:58 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -2,21 +2,25 @@
 echo "I am Frontend"
 
 set -e
+id =$(id -u)
 
+if["$id" -ne 0]; then
+    echo -e "\e[31m you need to be root user to execute this command or prefix sudo before the command \e[0m"
+    exit 1
 yum install nginx -y
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
+rm -rf * &>> /tmp/frontend
+unzip /tmp/frontend.zip &>> /tmp/frontend
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>> /tmp/frontend
+systemctl restart nginx &>> /tmp/frontend
 
 
 
