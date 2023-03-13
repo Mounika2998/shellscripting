b9b89786fdd43357afd0d20272d4137168dce4ee
Mounika2998 <mounikadamarla2429@gmail.com>
2023-03-13 22:49:07 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -12,15 +12,15 @@ yum install nginx -y
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
-rm -rf * &>> /tmp/frontend
-unzip /tmp/frontend.zip &>> /tmp/frontend
+rm -rf * 
+unzip /tmp/frontend.zip 
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx &>> /tmp/frontend
-systemctl restart nginx &>> /tmp/frontend
+systemctl enable nginx 
+systemctl restart nginx 
 
 
 
