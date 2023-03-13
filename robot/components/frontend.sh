9a91770b70a79416782acf7a22dbb063d0ecf330
Mounika2998 <mounikadamarla2429@gmail.com>
2023-03-13 23:37:48 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -8,50 +8,40 @@ if [ "$ID" -ne 0 ] ; then
     echo -e "\e[31m you need to be root user to execute this command or prefix sudo before the command \e[0m"
     exit 1
 
-fi  
-echo -n "Installing the Nginx:"  
-yum install nginx -y &>> /tmp/frontend
+fi 
 
-if [ $? -eq 0 ] ; then 
+stat() {
+    if [ $1 -eq 0 ] ; then 
         echo -e "\e[32m Success \e[0m"
     else 
         echo -e "\e[31m Failure \e[0m"
         exit 2
-fi 
+    fi 
+}
+
+echo -n "Installing the Nginx:"  
+yum install nginx -y &>> /tmp/frontend
+stat $?
+
+
 
 
 
 echo -n "Dowloading the frontend compoment:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-if [ $? -eq 0 ] ; then
-        echo -e "\e[32m Success \e[0m"
-    else
-        echo -e "\e[31m failure \e[0m"
-        exit 2
-
-fi
+stat $?
 echo -n "Performing the cleanup of old frontend component:"
 cd /usr/share/nginx/html
 rm -rf *  &>> /tmp/frontend
 
-if [ $? -eq 0 ] ; then
-        echo -e "\e[32m Success \e[0m"
-    else
-        echo -e "\e[31m failure \e[0m"
-        exit 2
-
-fi
+stat $?
 
 echo -n "copying the downloaded frontend content:"
 unzip /tmp/frontend.zip &>> /tmp/frontend
-if [ $? -eq 0 ] ; then
-        echo -e "\e[32m Sucess \e[0m"
-    else
-        echo -e "\e[31m failure \e[0m"
-        exit 2
 
-fi
+stat $?
+
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
@@ -62,13 +52,7 @@ echo -n "Starting the service:"
 systemctl enable nginx &>> /tmp/frontend
 systemctl restart nginx &>> /tmp/frontend
 
-if [ $? -eq 0 ] ; then
-        echo -e "\e[32m Sucess \e[0m"
-    else
-        echo -e "\e[31m failure \e[0m"
-        exit 2
-
-fi
+stat $?
 
 
 
