dbb5654779d2621992d833cd0275e0d61f13234e
Mounika2998 <mounikadamarla2429@gmail.com>
2023-03-14 23:26:50 +0530

    update

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -16,3 +16,10 @@ systemctl enable mysqld  &>> $LogFile
 systemctl start mysqld   &>> $LogFile
 stat $?
 
+echo -n "Grab the default password:"
+DEFAULT_ROOT_PWD= $(grep "temporary password"  var/log/mysqld.log | awk '{print $NF}')
+stat $?
+
+echo - n "Password reset of root user :"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LogFile
+stat $?
