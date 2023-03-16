6801c9134d4bb93026702299f737c6d20a5d2fd3
Mounika2998 <mounikadamarla2429@gmail.com>
2023-03-16 18:50:33 +0530

    update

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -14,9 +14,15 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Cen
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=B53-Allowall  | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
 echo -n "Ami ID is $AMI_ID"
 
+HOSTEDZONEID=Z0675622LCNBBN91K90Q
 echo -n "Launching the instance with $AMI_ID as AMI :"
 IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID \
                 --instance-type t3.micro \
                 --security-group-ids ${SGID} \
                 --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
                 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+
+
+sed -e "s/COMPONENT/${COMPONENT}/" -e  "s/IPADDRESS/${IPADDRESS}/" robot/record.json > /tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json | jq 
